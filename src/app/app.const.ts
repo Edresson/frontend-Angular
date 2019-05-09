764e1b600646e02cd70d5cdcae66edfb9068a9ba
Marcos Vieira <mavieira866@gmail.com>
2019-05-09 08:54:08 -0300

    key api map

--- src/app/app.const.ts
+++ src/app/app.const.ts
@@ -1,7 +1,9 @@
 export const API = {
-    //default: 'http://192.168.100.222:8080/adb-api-data/'
-    default: 'http://200.134.18.55:8080/adb-api-data/'
+    default: 'http://192.168.100.222:8080/adb-api-data/'
+    //default: 'http://200.134.18.55:8080/adb-api-data/'
 }
+
+//AIzaSyC3GsNDa6YcGMgcTw0e4tpSq5mSbC2Qz2s
 
 export function getDefaultURL(resource: string){
     return API.default + resource;
