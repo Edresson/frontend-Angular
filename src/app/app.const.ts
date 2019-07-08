f345a6bc1820ef3310414c125f4ef73ea27ef018
dresson <edresson1@gmail.com>
2019-07-08 14:22:18 -0300

    update

--- src/app/app.const.ts
+++ src/app/app.const.ts
@@ -1,6 +1,7 @@
 export const API = {
     //default: 'http://192.168.100.222:8080/adb-api-data/'
-    default: 'http://200.134.18.55:8080/adb-api-data/'
+    //default: 'http://200.134.18.55:8080/adb-api-data/'
+    default: 'http://agdatabox.md.utfpr.edu.br/apidata/v2/'
 }
 
 //AIzaSyC3GsNDa6YcGMgcTw0e4tpSq5mSbC2Qz2s
